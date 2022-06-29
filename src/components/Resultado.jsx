d435fbb0403779e924f72bc7663a8acfcf0eb0c2
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-06-29 13:16:02 -0500

    first commit

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  color: #fff;
+  font-family: "Lato", san-serif;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`;
+
+const Texto = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Precio = styled.p`
+  font-size: 26px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Imagen = styled.img`
+  display: block;
+  width: 120px;
+`;
+
+const Resultado = ({ resultado }) => {
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
+    resultado;
+
+  return (
+    <Contenedor>
+      <Imagen
+        src={`https://cryptocompare.com/${IMAGEURL}`}
+        alt="imagen cripto"
+      />
+      <div>
+        <Precio>
+          El precio es de: <span>{PRICE}</span>
+        </Precio>
+        <Texto>
+          El precio mas alto del dia es de: <span>{HIGHDAY}</span>
+        </Texto>
+        <Texto>
+          El precio mas bajo del dia es de: <span>{LOWDAY}</span>
+        </Texto>
+        <Texto>
+          Variacion ultimas 24HRS es de: <span>{CHANGEPCT24HOUR}</span>
+        </Texto>
+        <Texto>
+          Ultima actualizacion: <span>{LASTUPDATE}</span>
+        </Texto>
+      </div>
+    </Contenedor>
+  );
+};
+
+export default Resultado;
---
+++ src/data/moneda.js
@@ -0,0 +1,7 @@
+const monedas = [
+  { id: "USD", nombre: "Dolar Estados Unidos" },
+  { id: "COP", nombre: "Peso Colombiano" },
+  { id: "EUR", nombre: "Euro" },
+  { id: "GBP", nombre: "Libra Esterlina" },
+];
+export { monedas };
